9fb33132ff9a34feffe26f2921d70eb411b61a70
tehseen-kpr <tehseen.kpr@gmail.com>
2023-05-06 06:56:31 +0500

    login form validation completed

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   type:string="password";
   isText:boolean=false;
   eyeIcon:string="fa-eye-slash";
@@ -13,10 +14,44 @@ export class LoginComponent {
   /**
    *
    */
-  constructor() {
+
+  loginForm!:FormGroup; 
+
+  constructor(private fb:FormBuilder) {
     
   }
 
+
+  ngOnInit(): void {
+    this.loginForm=this.fb.group({
+      username:['',Validators.required],
+      password:['',Validators.required]
+    })  
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      
+    }
+    else{
+      this.validateAllFormFieled(this.loginForm);
+      alert('your form is invalid');
+      
+    }
+
+  }
+
+  private validateAllFormFieled(formGroup:FormGroup){
+    Object.keys(formGroup.controls).forEach(field=>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true});
+      }
+      else if(control instanceof FormGroup){
+        this.validateAllFormFieled(control)
+      }
+    })
+  }
 
   hideShowPass(){
      this.isText=!this.isText;
